72f4a8ceea1978cadf6171af46ecb860179f2bbd
nytxhlxq <704479663@qq.com>
2020-03-04 09:08:44 +0800

    list模拟实现

---
+++ 2020-2-28-C++/2020-2-28-C++/list模拟实现.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+namespace bit
+{
+	template<typename T>
+	struct ListNode
+	{
+
+	};
+	template<typename T>
+	class list
+	{
+
+	};
+};
+
+void main()
+{
+	bit::list<int> mylist;
+}
+//1:40:33
