cb926c6d5da9c0ac130f41cca8a8ea3cbf57d74e
nytxhlxq <704479663@qq.com>
2020-03-04 09:10:32 +0800

    牛课题第一天

---
+++ 牛课每日一题/牛课每日一题/test.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+char* test(char *ar1, char *ar2)
+{
+	char *p1 = ar1;
+	char *p2 = ar2;
+	char *p3 = p1;
+	while (*(p2) != '\0')
+	{
+		if (*(p3) == '\0')
+		{
+			p3 = p1;
+		}
+		while (*(p3) != '\0')
+		{
+			if (*(p2) == *(p3))
+			{
+				p3 = ++p3;
+				continue;
+			}	
+			*p3++;
+		}
+		++p2;
+	}
+	return ar1;
+}
+void main()
+{
+	char ar1[] = "they are students.";
+	char ar2[] = "aeiou";
+	char* ret = test(ar1,ar2);
+	cout << "string = " << ret << endl;
+	system("pause");
+}
