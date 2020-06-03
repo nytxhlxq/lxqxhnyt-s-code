38d03348bd5d9b78022402f58f4f2ef1a84d0bb8
nytxhlxq <704479663@qq.com>
2020-06-03 18:50:52 +0800

    6.1牛客

--- 牛课每日一题/牛课每日一题/test.cpp
+++ 牛课每日一题/牛课每日一题/test.cpp
@@ -3,6 +3,86 @@
 #include<string>
 #include<algorithm>
 using namespace std;
+//2020.6.1
+//1.美国接日
+int WeekToDay(int y, int m, int c, int w, bool B)
+{//输入哪年哪月第几个星期几，以及正数还是倒数，输出几号
+	int d, week, i;
+	if (m == 1)
+	{ 
+		m = 13;
+	    y--; 
+	}//公式要求1月、2月要转为上一年13、14月
+	if (m == 2)
+	{ 
+		m = 14;
+		y--; 
+	}
+	i = 0;
+	for (d = B ? 1 : 31; d <= B ? 31 : 1; B ? (d++) : (d--))
+	{//B：正数/倒数
+		week = (d + 2 * m + 3 * (m + 1) / 5 + y + y / 4 - y / 100 + y / 400) % 7;//蔡勒公式
+		if (week + 1 == w) 
+			++i;
+		if (i == c) 
+			break;
+	}
+	return d;
+}
+
+int main(void){
+	int y;
+	while (cin >> y)
+	{
+		cout << y << "-01-01" << endl;
+		printf("%d-01-%02d\n", y, WeekToDay(y, 1, 3, 1, 1));
+		printf("%d-02-%02d\n", y, WeekToDay(y, 2, 3, 1, 1));
+		printf("%d-05-%02d\n", y, WeekToDay(y, 5, 1, 1, 0));//倒数
+		cout << y << "-07-04" << endl;
+		printf("%d-09-%02d\n", y, WeekToDay(y, 9, 1, 1, 1));
+		printf("%d-11-%02d\n", y, WeekToDay(y, 11, 4, 4, 1));
+		cout << y << "-12-25" << endl << endl;
+	}
+	return 0;
+}
+//2.分解因数
+int main()
+{
+	int num;
+	while (cin >> num)
+	{
+		cout << num << " = ";
+		int* num1 = new int[1000];
+		int j = 0;
+		for (int i = 2; i <= sqrt(num); i++)
+		{
+			while (num % i == 0)
+			{
+				if (num != 1)
+				{
+					num1[j] = i;
+					j++;
+					num /= i;
+				}
+			}
+		}
+		if (num != 1)
+		{
+			num1[j] = num;
+			j++;
+		}
+		for (int k = 0; k < j; k++)
+		{
+			cout << num1[k];
+			if (k + 1 < j)
+			{
+				cout << " * ";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
 //3月2日
 //1.100449 组队竞赛
 int main()
