e8e6da026a0499b36d69abcf2884f906e04607fc
nytxhlxq <704479663@qq.com>
2020-06-06 12:02:49 +0800

    6.5 抄送列表

--- 牛课每日一题/牛课每日一题/test.cpp
+++ 牛课每日一题/牛课每日一题/test.cpp
@@ -3,6 +3,53 @@
 #include<string>
 #include<algorithm>
 using namespace std;
+//2020.6.5
+//抄送列表
+int main()
+{
+	string s, res;
+	while (getline(cin, s))
+	{
+		getline(cin, res);
+		bool sta = false;
+		for (int i = 0; i < s.length(); i++)
+		{
+			string t;
+			if (s[i] == '"')
+			{
+				i++;
+				while (i<s.length() && s[i] != '"')
+				{
+					t += s[i];
+					i++;
+				}
+				if (t == res)
+				{
+					sta = true;
+					break;
+				}
+			}
+			else if (s[i] != ',')
+			{
+				while (i<s.length() && s[i] != ',')
+				{
+					t += s[i];
+					i++;
+				}
+				if (t == res)
+				{
+					sta = true;
+					break;
+				}
+			}
+		}
+		if (sta == true)
+			cout << "Ignore" << endl;
+		else
+			cout << "Important!" << endl;
+	}
+	return 0;
+}
 //2020.6.4
 //1.养兔子
 #include<iostream>
