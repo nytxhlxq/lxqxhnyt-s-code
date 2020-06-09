50859d8b2f1611f208483b602aa9fc684dc5f8f6
nytxhlxq <704479663@qq.com>
2020-06-09 21:43:02 +0800

    6.8 蘑菇阵

--- 牛课每日一题/牛课每日一题/test.cpp
+++ 牛课每日一题/牛课每日一题/test.cpp
@@ -4,6 +4,37 @@
 #include<algorithm>
 #include <fstream>
 using namespace std;
+//2020.6.8
+//蘑菇阵
+int main()
+{
+	int n, m, k;
+	while (scanf("%d%d%d", &n, &m, &k) != EOF)
+	{
+		vector<vector<int>> table((n + 1), vector<int>(m + 1));
+		vector<vector<double>> P((n + 1), vector<double>(m + 1));
+		int x, y;
+		for (int i = 0; i < k; i++)
+		{
+			scanf("%d%d", &x, &y);
+			table[x][y] = 1;
+		}
+		P[1][1] = 1.0;//起点概率为1
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= m; j++)
+			{
+				if (!(i == 1 && j == 1))
+				{   
+					//跳过起点
+					P[i][j] = P[i - 1][j] * (j == m ? 1 : 0.5) + P[i][j - 1] * (i == n ? 1 : 0.5);//边界的时候，概率为1
+					if (table[i][j] == 1) P[i][j] = 0;//如果该点有蘑菇，概率置为0
+				}
+			}
+		}
+		printf("%.2lf\n", P[n][m]);
+	}
+}
 //2020.6.7
 //1.数据库连接池：相当于就是计算最大需要连接几次。
 int main()
