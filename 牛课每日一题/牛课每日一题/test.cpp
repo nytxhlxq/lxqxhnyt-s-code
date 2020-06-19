b13282a668a0cf94b796a5bfab13c446e67fcc85
nytxhlxq <704479663@qq.com>
2020-06-19 19:05:09 +0800

    出现次数超过一半的数字 1.排列取中间（非最优） 2.遍历并用累加器（优）

--- 牛课每日一题/牛课每日一题/test.cpp
+++ 牛课每日一题/牛课每日一题/test.cpp
@@ -5,6 +5,70 @@
 #include <fstream>
 #include<stack>
 using namespace std;
+//2020.6.19
+//出现次数超过一般的数（剑指offer也有）
+//1.超过一半意味着排列以后，位于中间的数必然是它,并非最优：
+class Solution {
+public:
+	int MoreThanHalfNum_Solution(vector<int> numbers)
+	{
+		// 因为用到了sort，时间复杂度O(NlogN)，并非最优
+		if (numbers.empty()) return 0;
+
+		sort(numbers.begin(), numbers.end()); // 排序，取数组中间那个数
+		int middle = numbers[numbers.size() / 2];
+
+		int count = 0; // 出现次数
+		for (int i = 0; i<numbers.size(); ++i)
+		{
+			if (numbers[i] == middle) ++count;
+		}
+
+		return (count>numbers.size() / 2) ? middle : 0;
+	}
+};
+//2.如果有符合条件的数字，，则它出现的次数比其他所有数字出现的次数和还要多。
+//  在遍历数组时保存两个值：一是数组中一个数字，一是次数。遍历下一个数字时，
+//  若它与之前保存的数字相同，则次数加1，否则次数减1；若次数为0，则保存下一个数字，并将次数置为1。
+//  遍历结束后，所保存的数字即为所求。然后再判断它是否符合条件即可。
+class Solution {
+public:
+	int MoreThanHalfNum_Solution(vector<int> numbers)
+	{
+		if (numbers.empty()) return 0;
+
+		// 遍历每个元素，并记录次数；若与前一个元素相同，则次数加1，否则次数减1
+		int result = numbers[0];
+		int times = 1; // 次数
+
+		for (int i = 1; i<numbers.size(); ++i)
+		{
+			if (times == 0)
+			{
+				// 更新result的值为当前元素，并置次数为1
+				result = numbers[i];
+				times = 1;
+			}
+			else if (numbers[i] == result)
+			{
+				++times; // 相同则加1
+			}
+			else
+			{
+				--times; // 不同则减1               
+			}
+		}
+
+		// 判断result是否符合条件，即出现次数大于数组长度的一半
+		times = 0;
+		for (int i = 0; i<numbers.size(); ++i)
+		{
+			if (numbers[i] == result) ++times;
+		}
+
+		return (times > numbers.size() / 2) ? result : 0;
+	}
+};
 //2020.6.17
 //求和（输入一个数，输出所有两数相加得它的情况）
 void help(int n, int m, vector<int>& v, int beg) 
